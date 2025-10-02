1cc161c2c9058900c7c07db6738d195ada03856b
Benjamin Badelles <bb201646@umconnect.umt.edu>
2025-10-02 11:58:48 -0600

    Add tests for products page

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class ProductsPageTests(TestCase):
+    def test_products_status_code(self):
+        """Products page should return HTTP 200"""
+        resp = self.client.get(reverse("products"))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_products_template_used(self):
+        """Products page should use products.html template"""
+        resp = self.client.get(reverse("products"))
+        self.assertTemplateUsed(resp, "products.html")
+
+    def test_products_context_has_four_items(self):
+        """Context should include exactly 4 products"""
+        resp = self.client.get(reverse("products"))
+        self.assertIn("products", resp.context)
+        self.assertEqual(len(resp.context["products"]), 4)
+
+    def test_products_content(self):
+        """Page should contain known product names"""
+        resp = self.client.get(reverse("products"))
+        self.assertContains(resp, "Griz Hoodie")
+        self.assertContains(resp, "Sticker Pack")
